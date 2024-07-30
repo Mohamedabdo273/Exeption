3e91623a7a669edd30ea1f131e2725e6a7535539
Mohamed abdo <159048705+Mohamedabdo273@users.noreply.github.com>
2024-07-30 20:59:39 +0300

    Add files via upload

---
+++ Program2.cs
@@ -0,0 +1,41 @@
+﻿namespace Task5_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                CheckForVowels("Hello");
+                CheckForVowels("bcd");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception: {ex.Message}");
+            }
+        }
+
+        static void CheckForVowels(string input)
+        {
+            char[] vowels = { 'a', 'e', 'i', 'o', 'u' };
+            bool containsVowel = false;
+            foreach (char e in vowels)
+            {
+                foreach (char i in input.ToLower())
+                {
+                    if (i == e)
+                    {
+                        containsVowel = true;
+                        break;
+                    }
+                }
+            }
+            if (!containsVowel)
+            {
+                throw new ArgumentException("The input string does not contain any vowels");
+            }
+
+            Console.WriteLine("The input string contains vowels.");
+        }
+    }
+}
